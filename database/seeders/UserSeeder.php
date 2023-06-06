463a7862a584cfee723d7fe1f945b8e86f59985e
trinhbang <huybang.trinh@secomus.com>
2023-06-06 22:56:22 +0700

    fix UserSeeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -16,16 +16,16 @@ class UserSeeder extends Seeder
     public function run()
     {
         DB::table('users')->insert([
-            ['name' => 'Nguyễn Thị Thu', 'sex' => 'female', 'email' => 'thunguyen98@gmail.com', 'password' => '123456', 'role' => 'teacher'],
-            ['name' => 'Trần Văn Minh', 'sex' => 'male', 'email' => 'minhtran99@gmail.com', 'password' => '123456', 'role' => 'teacher'],
-            ['name' => 'Lê Thị Ánh', 'sex' => 'female', 'email' => 'anhle98@gmail.com', 'password' => '123456', 'role' => 'teacher'],
-            ['name' => 'Phạm Đức Trung', 'sex' => 'male', 'email' => 'trungpham93@gmail.com', 'password' => '123456', 'role' => 'teacher'],
-            ['name' => 'Vũ Thị Mai Phương', 'sex' => 'female', 'email' => 'phuongvu97@gmail.com', 'password' => '123456', 'role' => 'teacher'],
-            ['name' => 'Hoàng Trung Hiếu ', 'sex' => 'male', 'email' => 'hieuhoang94@gmail.com', 'password' => '123456', 'role' => 'teacher'],
-            ['name' => 'Ngô Việt Bắc', 'sex' => 'male', 'email' => 'bacngo2k@gmail.com', 'password' => '123456', 'role' => 'teacher'],
-            ['name' => 'Nguyễn Đức Long', 'sex' => 'male', 'email' => 'longnguyen95@gmail.com', 'password' => '123456', 'role' => 'teacher'],
-            ['name' => 'Bùi Xuân Thủy', 'sex' => 'female', 'email' => 'thuybui91@gmail.com', 'password' => '123456', 'role' => 'teacher'],
-            ['name' => 'Trần Huy Phúc', 'sex' => 'male', 'email' => 'phuctran99@gmail.com', 'password' => '123456', 'role' => 'teacher']
+            ['name' => 'Nguyen Thi Thu', 'sex' => 'female', 'email' => 'thunguyen98@gmail.com', 'password' => '123456', 'role' => 'teacher'],
+            ['name' => 'Tran Van Minh', 'sex' => 'male', 'email' => 'minhtran99@gmail.com', 'password' => '123456', 'role' => 'teacher'],
+            ['name' => 'Le Thi Anh', 'sex' => 'female', 'email' => 'anhle98@gmail.com', 'password' => '123456', 'role' => 'teacher'],
+            ['name' => 'Pham Duc Trung', 'sex' => 'male', 'email' => 'trungpham93@gmail.com', 'password' => '123456', 'role' => 'teacher'],
+            ['name' => 'Vu Thi Mai Phuong', 'sex' => 'female', 'email' => 'phuongvu97@gmail.com', 'password' => '123456', 'role' => 'teacher'],
+            ['name' => 'Hoang Trung Hieu', 'sex' => 'male', 'email' => 'hieuhoang94@gmail.com', 'password' => '123456', 'role' => 'teacher'],
+            ['name' => 'Ngo Viet Bac', 'sex' => 'male', 'email' => 'bacngo2k@gmail.com', 'password' => '123456', 'role' => 'teacher'],
+            ['name' => 'Nguyen Duc Long', 'sex' => 'male', 'email' => 'longnguyen95@gmail.com', 'password' => '123456', 'role' => 'teacher'],
+            ['name' => 'Bui Xuan Thuy', 'sex' => 'female', 'email' => 'thuybui91@gmail.com', 'password' => '123456', 'role' => 'teacher'],
+            ['name' => 'Tran Huy Phuc', 'sex' => 'male', 'email' => 'phuctran99@gmail.com', 'password' => '123456', 'role' => 'teacher']
         ]);
     }
 }
