4eae02651e1883e0546b9c0c38f8311d7d71a418
bachhoang06060 <0606bach@gmail.com>
2023-07-12 21:48:15 +0700

    add new value time slot and aapi update user desire info

--- database/migrations/2023_06_05_101650_create_schedules_table.php
+++ database/migrations/2023_06_05_101650_create_schedules_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('schedules', function (Blueprint $table) {
             $table->id();
             $table->enum('day_of_week', ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']);
-            $table->enum('time_slot', [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]);
+            $table->enum('time_slot', [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]);
         });
     }
 
